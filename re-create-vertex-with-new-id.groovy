ac719e6a9ab998bb8cdc8c3e5723ceafbf2c30b6
Thirumal <m.thirumal@hotmail.com>
2021-07-15 21:06:10 +0530

    Recreate/Clone

--- re-create-vertex-with-new-id.groovy
+++ re-create-vertex-with-new-id.groovy
@@ -1,4 +1,5 @@
 //Re-Create vertex with new id. Clone vertex and move edges and delete the vertex.
 def reCreateV(g, old_id, new_id) {
-    return g.V(1).elementMap()
+    g.V(old_id).as('old_vertex').addV().property(label, select('old_vertex').label()).property(T.id, new_id).as('new_vertex').\
+    select('old_vertex').properties().as('old_vertex_properties').select('new_vertex').property(select('old_vertex_properties').key(), select('old_vertex_properties').value())
 }
